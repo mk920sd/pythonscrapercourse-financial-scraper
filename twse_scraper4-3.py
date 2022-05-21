90d53504e25874023d82eb6df6f48cb4ce75f01c
mk920sd <mk920sd@gmail.com>
2022-05-21 16:25:50 +0800

    4.3批次爬取JSON格式的網頁資料(GET)

--- twse_scraper4-3.py
+++ twse_scraper4-3.py
@@ -1,6 +1,6 @@
 import requests
 import json
 
-url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=json&date=20220519&selectType=30&_=1653011779435'
+url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=json&date=20220520&selectType=25&_=1653121065994'
 res = requests.get(url)
 print(res.json()['data'])
